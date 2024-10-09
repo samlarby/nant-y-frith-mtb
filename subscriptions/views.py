bb0ebc7f70505d85a352fe8138e4b856f2168b53
Sam Larby <samlarby1997@hotmail.com>
2024-10-09 21:19:39 +0000

    add stripe config view

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http.response import JsonResponse
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required
 def subscribe(request):
     return render(request, 'subscribe/subscribe.html')
 
+
+@csrf_exempt
+def stripe_config(request):
+    if request.method == 'GET':
+        stripe_config = {'publickey': settings.STRIPE_PUBLIC_KEY}
+        return JsonResponse(stripe_config, safe=False)
