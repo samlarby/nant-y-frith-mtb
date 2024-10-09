950ffa30da767c7e6cdd77c6f17dae7958a70e14
Sam Larby <samlarby1997@hotmail.com>
2024-10-09 21:14:33 +0000

    add static files urls

--- nant_y_frith_mtb/settings.py
+++ nant_y_frith_mtb/settings.py
@@ -133,10 +133,13 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [Path(BASE_DIR).joinpath('static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
