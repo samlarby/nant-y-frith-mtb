303bc79b019228e11279fe9c66f6e677fa4d55b9
Sam Larby <samlarby1997@hotmail.com>
2024-10-10 18:01:06 +0000

    add create checkout session

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,4 @@
+import stripe
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http.response import JsonResponse
@@ -15,3 +16,27 @@ def stripe_config(request):
     if request.method == 'GET':
         stripe_config = {'publickey': settings.STRIPE_PUBLIC_KEY}
         return JsonResponse(stripe_config, safe=False)
+
+
+@csrf_exempt
+def create_checkout_session(request):
+    if request.method == 'GET':
+        domain_url = 'http://localhost:8000/' # url define
+        stripe.api_key = settings.STRIPE_SECRET_KEY # automatically send request to create a new Checkout session
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                client_reference_id=request.user.id if request.user.is_authenticated else None,
+                success_url=domain_url + 'success?session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=domain_url + 'cancel/',
+                payment_method_types=['card'],
+                mode='subscriptions',
+                line_items=[
+                    {
+                        'price': settings.STRIPE_PRICE_ID,
+                        'quantity': 1, 
+                    }
+                ]
+            )
+            return JsonResponse({'sessionId': checkout_session['id']})
+        except Exception as e:
+            return JsonResponse({'error': str(e)})
