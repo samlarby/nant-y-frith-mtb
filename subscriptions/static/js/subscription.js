6540ec9172cb64990c7f8174d313f6adc2540b30
Sam Larby <samlarby1997@hotmail.com>
2024-10-10 18:13:10 +0000

    add event handler for ajax request

--- subscriptions/static/js/subscription.js
+++ subscriptions/static/js/subscription.js
@@ -4,4 +4,22 @@ fetch("/config/")
 .then((result) => {return result.json(); })
 .then((data) => {
     const stripe = Stripe(data.publicKey)
+
+    let submitBtn = document.querySelector("#submitBtn");
+    if (submitBtn !== null) {
+        submitBtn.addEventListener("click", () => {
+        // get checkout session id
+        fetch("/create-checkout-session/")
+            .then((result) => { return result.json(); })
+            .then((data) => {
+                console.log(data);
+                // redirect to stripe checkout
+                return stripe.redirectToCheckout({sessionId: data.sessionId})
+            })
+            .then((res) => {
+                console.log(res);
+            });
+        });
+    }
 });
+
