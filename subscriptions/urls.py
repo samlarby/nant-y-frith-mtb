280d9ee5a951795264dd23f97c421865c3eb9908
Sam Larby <samlarby1997@hotmail.com>
2024-10-10 18:03:18 +0000

    add create checkout session url

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.subscribe, name='subscriptions-subscribe'),
-    path('config/', views.stripe_config)
+    path('config/', views.stripe_config),
+    path('create-checkout-session/', views.create_checkout_session),
 ]
 
