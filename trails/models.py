46df2d3ef937ad68daee8e887bc65525dbcbb413
Sam Larby <samlarby1997@hotmail.com>
2024-10-13 18:32:15 +0000

    add basic template, views and models

--- trails/models.py
+++ trails/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Trail(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    difficulty = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- trails/views.py
+++ trails/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Trail
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def trails(request):
+    """Query the database for Trail objects
+       Then pass the objects to the trails template
+    """
+    trails = Trail.objects.all()
+
+    # check if user is subscribed through the profile app
+    user_is_subscribed = request.user.id if request.user.is_authenticated else False
+
+    return render(request, 'trails/trails.html', {
+      'trails': trails,
+      'user_is_subscribed': user_is_subscribed,
+    })
