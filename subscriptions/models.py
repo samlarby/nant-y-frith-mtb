00fbdb8eb7abe36923b7ab42eff3e2ac035024d9
Sam Larby <samlarby1997@hotmail.com>
2024-10-13 20:09:48 +0000

    edit model and view to see if user is described

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.conf import settings
+import stripe
 
 class StripeCustomer(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     stripeCustomerId = models.CharField(max_length=255)
     stripeSubscriptionId = models.CharField(max_length=255)
+
+    def has_active_subscription(self):
+        """
+        Returns True if the customer has an active subscription, False otherwise.
+        """
+        if not self.stripeSubscriptionId:
+            return False
+
+        try:
+            stripe.api_key = settings.STRIPE_SECRET_KEY
+            subscription = stripe.Subscription.retrieve(self.stripeSubscriptionId)
+            return subscription.status == 'active'
+        except stripe.error.StripeError:
+            return False  # Safely handle Stripe API errors
 
     def __str__(self):
         return self.user.username
--- trails/views.py
+++ trails/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Trail
 from django.contrib.auth.models import User
-
+from subscriptions.models import StripeCustomer
 
 def trails(request):
     """Query the database for Trail objects
@@ -9,8 +9,16 @@ def trails(request):
     """
     trails = Trail.objects.all()
 
-    # check if user is subscribed through the profile app
-    user_is_subscribed = request.user.id if request.user.is_authenticated else False
+    # check if user is subscribed through the subscriptions app
+     # Check if the user is subscribed (if they are authenticated)
+    user_is_subscribed = False  # Default to not subscribed
+    if request.user.is_authenticated:
+        try:
+            # Retrieve the StripeCustomer object for the user and check subscription status
+            stripe_customer = StripeCustomer.objects.get(user=request.user)
+            user_is_subscribed = stripe_customer.has_active_subscription()
+        except StripeCustomer.DoesNotExist:
+            user_is_subscribed = False
 
     return render(request, 'trails/trails.html', {
       'trails': trails,
