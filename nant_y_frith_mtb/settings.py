162b14bafb95fa73b2da83053640c32e01f88ad1
Sam Larby <samlarby1997@hotmail.com>
2024-10-13 17:27:21 +0000

    add trails app, add to installed apps and urls

--- nant_y_frith_mtb/settings.py
+++ nant_y_frith_mtb/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'subscriptions.apps.SubscriptionsConfig', 
 
     'home',
+    'trails',
+    
     
 ]
 
--- nant_y_frith_mtb/urls.py
+++ nant_y_frith_mtb/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('subscriptions.urls')), 
     path('accounts/', include('allauth.urls')),
     path('home/', include('home.urls')),
+    path('trails/', include('trails.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
