449ffa5929405f03b1407ef77bde37fa29c33049
Sam Larby <samlarby1997@hotmail.com>
2024-10-15 22:50:40 +0000

    edit to page structures and styles

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.subscribe, name='subscriptions-subscribe'),
-    path('stripe_config/', views.stripe_config, name="stripe_config"),
+    path('stripe_config/', views.stripe_config, name='stripe_config'),
     path('create_checkout_session/', views.create_checkout_session, name='create_checkout_session'),
     path('success/', views.success, name='success'),
     path('cancel/', views.cancel, name='cancel'),
-    path('webhook/', views.stripe_webhook, name="webhook"),
+    path('webhook/', views.stripe_webhook, name='webhook'),
 ]
 
